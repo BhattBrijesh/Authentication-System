30a33746d62002e4e1d473928b335385c1bbd8b8
brijesh.bhatt <brijesh.bhatt@onpointwarranty.com>
2026-04-22 22:00:36 +0530

    logout all controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -187,3 +187,79 @@ export async function logout(req, res) {
         throw new Error(err)
     }
 }
+
+export async function logoutAll(req, res) {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) {
+            return res.status(400).json({
+                message: "Refresh token not found"
+            })
+        }
+        const decode = jwt.verify(refreshToken, JWT_SECRET)
+        await sessionModel.updateMany({
+            user: decode.id,
+            revoked: false
+        }, {
+            revoked: true
+        })
+        res.clearCookie('refreshToken')
+        res.status(200).json({
+            message: "Logged out from all devices successfully"
+        })
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+export async function login(req, res) {
+    try {
+        const { email, password } = req.body
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            })
+        }
+        const hashedPassword = crypto.createHash("sha256").update(password).digest('hex');
+        const isPassword = hashedPassword === user.password;
+        if (!isPassword) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            })
+        }
+        const refreshToken = jwt.sign({
+            id: user._id
+        }, JWT_SECRET, {
+            expiresIn: "7d"
+        })
+        const refreshTokenHash = crypto.createHash('sha256').update(refreshToken).digest('hex');
+        const session = await sessionModel.create({
+            user: user._id,
+            refreshTokenHash,
+            ip: req.ip,
+            userAgent: req.headers['user-agent']
+        })
+        const accessToken = jwt.sign({
+            id: user._id,
+            sessionId: session._id
+        }, JWT_SECRET, { expiresIn: "15m" }
+        )
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        res.status(200).json({
+            message: "Logged in successfully",
+            user: {
+                userName: user.userName,
+                email: user.email
+            },
+            accessToken
+        })
+    } catch (error) {
+        throw new Error(error)
+    }
+}
